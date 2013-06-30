2cad02b1495d6ee29d5aff158874588e2bfc5006
Ivan Paner <ivan.paner@gmail.com>
2013-06-30 14:36:24 +0800

    Removed allLogs(). Probably not needed yet.

--- Information Asset Registry/src/model/bean/Classification.java
+++ Information Asset Registry/src/model/bean/Classification.java
@@ -99,45 +99,5 @@ public class Classification {
         return latest;
     }
     
-    // temporary thing. don't copy
-    public static Vector<Log> allLogs(User user) {
-        Connection conn = DBUtil.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        Vector<Log> allLogs = new Vector<>();
-        try {
-            ps = conn.prepareStatement(
-                "SELECT date(dateTime) AS date, identifier, " +
-                "    time(dateTime) AS time, classification " +
-                "FROM Log " +
-                "    JOIN Asset ON assetFk = Asset.pk " +
-                "    JOIN Classification ON attributeFk = Classification.pk " +
-                "WHERE userFk = 1 AND attribute = 'Classification' "
-            );
-            ps.setInt(1, user.pk());
-            rs = ps.executeQuery();
-            
-            while (rs.next()) {
-                Log currentLog = new Log();
-                Time time = rs.getTime("time");
-                currentLog.setTime(time);
-                Date date = rs.getDate("date");
-                currentLog.setDate(date);
-                currentLog.setAction("UPDATED");
-                allLogs.add(currentLog);
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            DBUtil.close(rs);
-            DBUtil.close(ps);
-            DBUtil.close(conn);
-        }
-        
-        Collections.sort(allLogs);
-        return allLogs;
-    }
+
 }
