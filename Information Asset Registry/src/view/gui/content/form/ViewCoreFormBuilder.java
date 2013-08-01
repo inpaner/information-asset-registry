9a157950de7ecb2d00091481a5c83fdb0d6be8c9
Darren Sapalo <darren.sapalo@gmail.com>
2013-08-01 22:24:04 +0800

    Applied the setEditable changes between View and Edit core forms.

--- Information Asset Registry/src/view/gui/content/form/ViewCoreFormBuilder.java
+++ Information Asset Registry/src/view/gui/content/form/ViewCoreFormBuilder.java
@@ -2,6 +2,12 @@ package view.gui.content.form;
 
 import java.util.ArrayList;
 
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
+import org.jdesktop.swingx.JXDatePicker;
+
 import view.gui.content.Content;
 import view.gui.content.CoreForm;
 import view.gui.content.form.field.Field;
@@ -16,13 +22,26 @@ public class ViewCoreFormBuilder extends AddCoreFormBuilder{
 	@Override
 	public Content buildContent() {
 		content = super.buildContent();
-		
+		setFieldsEditable(false);			
+		return content;
+	}
+	
+	protected void setFieldsEditable(boolean b){
 		CoreForm form = (CoreForm)content;
 		ArrayList<Field>fields = form.getFields();
-		for (Field f : fields)
+		for (Field f : fields){
 			f.getInput().initialize();
+			JComponent component = f.getInput().getComponent();
 			
-		return content;
+			// Disable
+			if (component instanceof JTextField){
+				((JTextField) component).setEditable(b); 
+			}else if (component instanceof JComboBox){
+				((JComboBox) component).setEnabled(b);
+			}else if (component instanceof JXDatePicker){
+				((JXDatePicker) component).setEditable(b);
+			}
+		}
 	}
 	
 }
